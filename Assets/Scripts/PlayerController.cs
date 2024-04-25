ce8babdaac09d1bce0130a9f41ec93117c0f034f
Rishabh Garg <narishabh.garg@gmail.com>
2024-04-25 14:46:04 +0530

    Added Crouch and Jump Animations
    Added Crouch and Jump Animations and their collider modification code as well for Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float moveSpeed = 0.0f;
-    public Animator animator;
     bool isFacingLeft = false;
-    // Update is called once per frame
+    bool isCrouching = false;
+    bool isGrounded = true;
+    float moveSpeed = 0.0f;
+    float jumpForce = 0.0f;
+    float crouchingHeightRatio = 0.6f;
+    float crouchingWidthSizeRatio = 0.7f;
+    float crouchingWidthOffsetRatio = .025f;
+    Vector2 colliderOffsetOriginal = Vector2.zero;
+    Vector2 colliderSizeOriginal = Vector2.zero;
+
+    float jumpingHeightRatio = 0.7f;
+    float afterJumpWaitInSeconds = 0.4f;
+
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.1f;
+
+    public Animator animator;
+    private CapsuleCollider2D capsuleCollider2D;
+
+
+    void Start()
+    {
+        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        colliderSizeOriginal = capsuleCollider2D.size;
+        colliderOffsetOriginal = capsuleCollider2D.offset;
+    }
     void Update()
     {
         moveSpeed = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = GroundCheck();
+
+        if (Input.GetAxisRaw("Vertical") > 0.0f && isGrounded && !isCrouching && jumpForce == 0.0f)
+        {
+            jumpForce = afterJumpWaitInSeconds;
+            capsuleCollider2D.transform.position = new Vector3(capsuleCollider2D.transform.position.x, capsuleCollider2D.transform.position.y + jumpingHeightRatio, 0.0f);
+        }
+        jumpForce -= Time.deltaTime;
+        if (jumpForce < 0.0f && !isCrouching)
+        {
+            jumpForce = 0.0f;
+        }
+    
+        animator.SetFloat("jumpForce", jumpForce);       
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+            if (isCrouching)
+            {
+                capsuleCollider2D.size = new Vector2(capsuleCollider2D.size.x / crouchingWidthSizeRatio, capsuleCollider2D.size.y * crouchingHeightRatio);
+                capsuleCollider2D.offset = new Vector2(capsuleCollider2D.offset.x / crouchingWidthOffsetRatio, capsuleCollider2D.offset.y * crouchingHeightRatio);
+            }
+            else {
+                capsuleCollider2D.size = colliderSizeOriginal;
+                capsuleCollider2D.offset = colliderOffsetOriginal;
+            }
+            animator.SetBool("isCrouching", isCrouching);
+        }
+
 
         if (moveSpeed < 0.0f)
         {
@@ -34,6 +89,14 @@ public class PlayerController : MonoBehaviour
 
         animator.SetFloat("moveSpeed", moveSpeed);
 
-        
+    }
+
+    bool GroundCheck()
+    {
+        // Cast a ray downward to check for ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+
+        // If the ray hits something on the ground layer, return true
+        return hit.collider != null;
     }
 }
